e1aea78d103a221e4cbc1caa8f073aabdb0aaf86
Usman Haider Khan <usmanhaiderkhan4@gmail.com>
2019-02-05 20:39:31 +0500

    Update Added Js Files

---
+++ Medical-five/assets/Bootstrap4/js/script.js
@@ -0,0 +1,119 @@
+﻿//console.log("usman");
+
+/*================== Read More Text ==================*/
+$(function () {
+    var showChar = 110;
+    var moretext = "Know More";
+    var lesstext = "Know Less";
+    $('.comments-space').each(function () {
+        var content = $(this).html();
+        if (content.length > showChar) {
+            var show_content = content.substr(0, showChar);
+            var hide_content = content.substr(showChar, content.length - showChar);
+            var html = show_content + '<span class="remaining-contents" style="display:block"><span>' + hide_content +
+                '</span><a href="" class="morelinks btn btn-know mt-45 text-center media-ml"  style="display:block;margin-left:26%;">' + moretext + "<span class='fas fa-arrow-right'></span>" + '</a>'
+                + '</span>';
+            $(this).html(html);
+        }
+    });
+
+    $(".morelinks").click(function () {
+
+        if ($(this).hasClass("less")) {
+            $(this).removeClass("less");
+            $(this).html(moretext);
+        } else {
+            $(this).addClass("less");
+            $(this).html(lesstext);
+        }
+        $(this).parent().prev().toggle();
+        $(this).prev().toggle();
+        return false;
+    });
+});
+
+
+/*===================== Load More Images ======================*/
+$(function () {
+    $('a').smoothScroll();
+});
+/*======================= Nav Active Class =======================*/
+
+$(function () {
+    $('.nav-link').on('click',
+        function () {
+            $('.nav-link').removeClass('active');
+            $(this).addClass('active');
+        });
+});
+
+
+
+/* ==================== Js Function to give the class on Scroll ================== */
+$(function () {
+    $(window).scroll(function () {
+        var scroll = $(window).scrollTop();
+
+        if (scroll >= 100) {
+            $("nav").addClass("fixed-top sleep");
+        } else {
+            $("nav").removeClass("fixed-top sleep");
+        }
+    });
+});
+
+
+/*===================== Load More Images ======================*/
+$(document).ready(function () {
+
+    $('.loadMore').loadMoreResults({
+        displayedItems: 6,
+        showItems: 3
+
+    });
+
+});
+
+/*===================== Date Picker Function ======================*/
+
+$(function () {
+    $('.responsive').slick({
+        dots: false,
+        infinite: true,
+        speed: 300,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1100,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3
+                }
+            },
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
+    });
+});
